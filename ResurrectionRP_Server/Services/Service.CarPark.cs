4d2a4e021efda663c2190325bb5e752cb1ccf7a1
Unknown <alexandre.asto63@orange.fr>
2019-08-22 22:12:00 +0200

    temporis

---
+++ ResurrectionRP_Server/Services/Service.CarPark.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading.Tasks;
+using AltV.Net;
+using AltV.Net.Elements.Entities;
+
+namespace ResurrectionRP_Server.Services
+{
+    class CarPark
+    {
+        #region Variables
+        [BsonId]
+        public int ID;
+
+        //public Models.Parking Parking;
+
+        #endregion
+    }
+}

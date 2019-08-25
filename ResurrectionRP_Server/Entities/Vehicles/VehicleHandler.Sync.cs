50a378de42321318cbe5fa46b30580526721e58d
Unknown <alexandre.asto63@orange.fr>
2019-08-25 12:41:01 +0200

    Rollback vehicle properties

--- ResurrectionRP_Server/Entities/Vehicles/VehicleHandler.Sync.cs
+++ ResurrectionRP_Server/Entities/Vehicles/VehicleHandler.Sync.cs
@@ -2,10 +2,18 @@
 using AltV.Net.Enums;
 using ResurrectionRP.Entities.Vehicles.Data;
 using ResurrectionRP_Server.Entities.Vehicles.Data;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Options;
 using ResurrectionRP_Server.Models;
+using ResurrectionRP_Server.Utils.Extensions;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Numerics;
 using System.Threading.Tasks;
-
+using VehicleInfoLoader.Data;
 namespace ResurrectionRP_Server.Entities.Vehicles
 {
     public enum WindowID
@@ -74,7 +82,33 @@ namespace ResurrectionRP_Server.Entities.Vehicles
         public float PowerMultiplicator { get; set; }
         public float Milage { get; set; }
 
+        [BsonDictionaryOptions(DictionaryRepresentation.ArrayOfArrays)]
+        public ConcurrentDictionary<int, int> Mods { get; set; }
+= new ConcurrentDictionary<int, int>();
+
+        public uint BodyHealth { get; set; } = 1000;
+        public int EngineHealth { get; set; } = 1000;
+        public int PetrolTankHealth { get; set; }
+
+        public Tuple<bool, bool, bool, bool> NeonState { get; set; } = new Tuple<bool, bool, bool, bool>(false, false, false, false);
         public Color NeonsColor { get; set; } = Color.Empty;
+
+        public byte Dirt { get; set; } = 0;
+        public bool Engine { get; set; } = false;
+
+
+        public byte PrimaryColor { get; set; } = 0;
+        public byte SecondaryColor { get; set; } = 0;
+
+        public byte WindowTint { get; set; } = 0;
+        public bool ArmoredWindows { get; set; } = false;
+
+        public byte SirenActive { get; set; } = 0;
+
+        public byte FrontBumperDamage { get; set; } = 0;
+        public byte RearBumperDamage { get; set; } = 0;
+
+
         public VehicleDoorState[] Door { get; set; } = new VehicleDoorState[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
         public WindowState[] Window { get; set; } = new WindowState[4] { 0, 0, 0, 0 };
         public WheelState[] Wheel { get; set; } = new WheelState[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };

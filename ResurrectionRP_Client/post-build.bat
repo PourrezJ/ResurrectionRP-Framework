289539152d707f41461cc72583cdb29fd18da453
Le Shred <fred777@free.fr>
2019-08-28 18:19:14 +0200

    New client-side build management

---
+++ ResurrectionRP_Client/post-build.bat
@@ -0,0 +1,16 @@
+﻿@echo off
+set ClientDir=E:\Sources\AltV\Server-Beta\resources\resurrectionrp\client
+set TargetDir=E:\Sources\AltV\ResurrectionRP\ResurrectionRP_Client\bin
+set ProjectDir=E:\Sources\AltV\ResurrectionRP\ResurrectionRP_Client
+
+del %TargetDir%\*.dll
+del %TargetDir%\*.pdb
+rmdir /S /Q %ClientDir%
+mkdir %ClientDir%
+mkdir %ClientDir%\cef
+mkdir %ClientDir%\lib
+mkdir %ClientDir%\NativeUIMenu
+xcopy /E /Y %TargetDir% %ClientDir%
+xcopy /E /Y %ProjectDir%\cef %ClientDir%\cef
+xcopy /E /Y %ProjectDir%\lib %ClientDir%\lib
+xcopy /E /Y %ProjectDir%\client\NativeUIMenu %ClientDir%\NativeUIMenu

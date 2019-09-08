0263a4c98e7043d5f4e3258ef8be87ec0c2c4908
Djoe_ <43145127+PourrezJ@users.noreply.github.com>
2019-09-08 09:33:07 +0200

    Weather async to sync methods

--- ResurrectionRP_Server/Weather/WeatherManager.cs
+++ ResurrectionRP_Server/Weather/WeatherManager.cs
@@ -19,7 +19,7 @@ namespace ResurrectionRP_Server.Weather
         public float WeatherTransition { get; set; }
         public bool Forced { get; set; }
 
-        public async Task InitWeather()
+        public Task InitWeather()
         {
             try
             {
@@ -34,18 +34,19 @@ namespace ResurrectionRP_Server.Weather
                 var client = new OpenWeatherAPI.OpenWeatherAPI(apikey);
 
                 var results = client.Query(Config.GetSetting<string>("OpenWeatherCity"));
-                await WeatherDataReceived(results.Weathers[0], results.Wind);
+                WeatherDataReceived(results.Weathers[0], results.Wind);
 
-                Utils.Utils.Delay((int)TimeSpan.FromMinutes(5).TotalMilliseconds, false, async () =>
+                Utils.Utils.Delay((int)TimeSpan.FromMinutes(5).TotalMilliseconds, false, () =>
                 {
                     results = client.Query(Config.GetSetting<string>("OpenWeatherCity"));
-                    await WeatherDataReceived(results?.Weathers[0], results?.Wind);
+                    WeatherDataReceived(results?.Weathers[0], results?.Wind);
                 });
             }
             catch (Exception ex)
             {
                 Alt.Server.LogError("InitWeather" + ex.ToString());
             }
+            return Task.CompletedTask;
         }
 
         public void UpdatePlayersWeather()
@@ -54,13 +55,13 @@ namespace ResurrectionRP_Server.Weather
         }
 
         private Timer timer = null;
-        public async Task ChangeWeather(WeatherType weather, double wind, double winddirection)
+        public void ChangeWeather(WeatherType weather, double wind, double winddirection)
         {
             if (Forced)
                 return;
 
             this.Wind = wind;
-            this.WindDirection = winddirection / 4;
+            this.WindDirection = winddirection;
 
             if (timer != null)
             {
@@ -72,7 +73,6 @@ namespace ResurrectionRP_Server.Weather
             {
                 this.Actual_weather = weather;
                 WeatherTransition = 0.0f;
-#pragma warning disable CS4014 // Dans la mesure où cet appel n'est pas attendu, l'exécution de la méthode actuelle continue avant la fin de l'appel
                 timer = Utils.Utils.Delay(2500, false, async () =>
                 {
                     WeatherTransition += 0.02f;
@@ -82,12 +82,10 @@ namespace ResurrectionRP_Server.Weather
                         WeatherTransition = 1;
                         timer.Close();
                     }
-                    //await .CallAsync("WeatherManager_Change", weather.ToString(), wind, winddirection, WeatherTransition);
                     this.UpdatePlayersWeather();
 
                     await Task.Delay(2500);
                 });
-#pragma warning restore CS4014 // Dans la mesure où cet appel n'est pas attendu, l'exécution de la méthode actuelle continue avant la fin de l'appel
                 Alt.Server.LogInfo("[WEATHER] Weather Update to " + weather.ToString());
             }
             else if (WeatherTransition > 1.0)
@@ -96,16 +94,16 @@ namespace ResurrectionRP_Server.Weather
             }
         }
 
-        private async Task WeatherDataReceived(OpenWeatherAPI.Weather weather, Wind wind)
+        private void WeatherDataReceived(OpenWeatherAPI.Weather weather, Wind wind)
         {
             if (wind == null || weather == null) return;
             switch (weather.ID)
             {
                 case 200:
-                    await ChangeWeather(WeatherType.Overcast, wind.SpeedMetersPerSecond, wind.Degree);
+                    ChangeWeather(WeatherType.Overcast, wind.SpeedMetersPerSecond, wind.Degree);
                     break;
                 case 201:
-                    await ChangeWeather(WeatherType.Rain, wind.SpeedMetersPerSecond, wind.Degree);
+                    ChangeWeather(WeatherType.Rain, wind.SpeedMetersPerSecond, wind.Degree);
                     break;
                 case 202:
                 case 210:
@@ -115,27 +113,27 @@ namespace ResurrectionRP_Server.Weather
                 case 230:
                 case 231:
                 case 232:
-                    await ChangeWeather(WeatherType.Thunder, wind.SpeedMetersPerSecond, wind.Degree);
+                    ChangeWeather(WeatherType.Thunder, wind.SpeedMetersPerSecond, wind.Degree);
                     break;
 
 
                 case 800:
-                    await ChangeWeather(WeatherType.Clear, wind.SpeedMetersPerSecond, wind.Degree);
+                    ChangeWeather(WeatherType.Clear, wind.SpeedMetersPerSecond, wind.Degree);
                     break;
 
                 case 801:
                 case 802:
                 case 803:
                 case 804:
-                    await ChangeWeather(WeatherType.Clouds, wind.SpeedMetersPerSecond, wind.Degree);
+                    ChangeWeather(WeatherType.Clouds, wind.SpeedMetersPerSecond, wind.Degree);
                     break;
 
                 case 500:
-                    await ChangeWeather(WeatherType.Clearing, wind.SpeedMetersPerSecond, wind.Degree);
+                    ChangeWeather(WeatherType.Clearing, wind.SpeedMetersPerSecond, wind.Degree);
                     break;
 
                 case 501:
-                    await ChangeWeather(WeatherType.Overcast, wind.SpeedMetersPerSecond, wind.Degree);
+                    ChangeWeather(WeatherType.Overcast, wind.SpeedMetersPerSecond, wind.Degree);
                     break;
 
                 case 502:
@@ -146,12 +144,12 @@ namespace ResurrectionRP_Server.Weather
                 case 521:
                 case 522:
                 case 531:
-                    await ChangeWeather(WeatherType.Rain, wind.SpeedMetersPerSecond, wind.Degree);
+                    ChangeWeather(WeatherType.Rain, wind.SpeedMetersPerSecond, wind.Degree);
                     break;
 
                 case 300:
                 case 301:
-                    await ChangeWeather(WeatherType.Smog, wind.SpeedMetersPerSecond, wind.Degree);
+                    ChangeWeather(WeatherType.Smog, wind.SpeedMetersPerSecond, wind.Degree);
                     break;
 
                 case 302:
@@ -161,7 +159,7 @@ namespace ResurrectionRP_Server.Weather
                 case 313:
                 case 314:
                 case 321:
-                    await ChangeWeather(WeatherType.Foggy, wind.SpeedMetersPerSecond, wind.Degree);
+                    ChangeWeather(WeatherType.Foggy, wind.SpeedMetersPerSecond, wind.Degree);
                     break;
 
                 case 701:
@@ -174,11 +172,11 @@ namespace ResurrectionRP_Server.Weather
                 case 762:
                 case 771:
                 case 781:
-                    await ChangeWeather(WeatherType.Foggy, wind.SpeedMetersPerSecond, wind.Degree);
+                    ChangeWeather(WeatherType.Foggy, wind.SpeedMetersPerSecond, wind.Degree);
                     break;
 
                 case 600:
-                    await ChangeWeather(WeatherType.Snowlight, wind.SpeedMetersPerSecond, wind.Degree);
+                    ChangeWeather(WeatherType.Snowlight, wind.SpeedMetersPerSecond, wind.Degree);
                     break;
 
                 case 601:
@@ -190,10 +188,10 @@ namespace ResurrectionRP_Server.Weather
                 case 620:
                 case 621:
                 case 622:
-                    await ChangeWeather(WeatherType.Snow, wind.SpeedMetersPerSecond, wind.Degree);
+                    ChangeWeather(WeatherType.Snow, wind.SpeedMetersPerSecond, wind.Degree);
                     break;
                 default:
-                    await ChangeWeather(WeatherType.Clear, 0, 0);
+                    ChangeWeather(WeatherType.Clear, 0, 0);
                     break;
             }
         }

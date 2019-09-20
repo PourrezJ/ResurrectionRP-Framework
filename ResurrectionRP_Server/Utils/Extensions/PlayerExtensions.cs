00cb5987fbac86747141fb58554310742d6b3ed5
alburgoni <alexandre.asto63@orange.fr>
2019-09-20 19:42:57 +0200

    Fix Ped Hair Variation (Fuck playercustom)

--- ResurrectionRP_Server/Utils/Extensions/PlayerExtensions.cs
+++ ResurrectionRP_Server/Utils/Extensions/PlayerExtensions.cs
@@ -274,22 +274,23 @@ namespace ResurrectionRP_Server
 
         public static Task SetHeadOverlayAsync(this IPlayer client, int overlayId, Business.Barber.HeadOverlayData overlayData)
         {
-            // TODO
+            client.EmitLocked("HeadOverlayVariation", overlayData.Index, overlayData.Opacity, overlayData.ColorId, overlayData.SecondaryColorId, overlayId);
             return Task.CompletedTask;
         }
 
         public static void SetHeadOverlay(this IPlayer client, int overlayId, Business.Barber.HeadOverlayData overlayData)
         {
-            // TODO
+            client.Emit("HeadOverlayVariation", overlayData.Index, overlayData.Opacity, overlayData.ColorId, overlayData.SecondaryColorId, overlayId);
         }
 
-        public static void SetHairColor(this IPlayer client, uint color, uint highlightColor)
+        public static void SetHairColor(this IPlayer client, uint color, uint hightlightColor)
         {
-            // TOOD
+            client.Emit("HairVariation", color, hightlightColor);
         }
 
         public static Task SetHairColorAsync(this IPlayer client,  uint color, uint hightlightColor)
         {
+            client.EmitLocked("HairVariation", color, hightlightColor);
             return Task.CompletedTask;
         }
 

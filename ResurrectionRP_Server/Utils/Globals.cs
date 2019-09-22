017afc407c6871a8a83034b4408ad2fa7f9f8565
Le Shred <fred777@free.fr>
2019-09-22 19:52:30 +0200

    Update number of doors

--- ResurrectionRP_Server/Utils/Globals.cs
+++ ResurrectionRP_Server/Utils/Globals.cs
@@ -8,7 +8,7 @@ namespace ResurrectionRP_Server.Utils
         public const Menu.MenuAnchor MENU_ANCHOR = Menu.MenuAnchor.TopRight;
         public const int MENU_POSX = -34;
         public const int MENU_POSY = 120;
-        public const int NB_VEHICLE_DOORS = 6;
+        public const byte NB_VEHICLE_DOORS = 8;
         public const int NB_VEHICLE_WINDOWS = 4;
         public const double MAX_INTERACTION_DISTANCE = 2.0f;
     }

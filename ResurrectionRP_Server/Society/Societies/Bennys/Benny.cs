2d13dc2c7ea6b0fd7d2f8813bb92f35c11d2bdd0
Le Shred <fred777@free.fr>
2019-09-28 01:17:41 +0200

    Fix window tint not working properly

--- ResurrectionRP_Server/Society/Societies/Bennys/Benny.cs
+++ ResurrectionRP_Server/Society/Societies/Bennys/Benny.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AltV.Net;
+using AltV.Net.Async;
+using AltV.Net.Elements.Entities;
+using AltV.Net.Enums;
+using ResurrectionRP_Server.Entities.Peds;
+using ResurrectionRP_Server.Models;
 using System.Numerics;
 using System.Threading.Tasks;
-using AltV.Net.Elements.Entities;
-using ResurrectionRP_Server.Models;
-using AltV.Net.Enums;
-using AltV.Net;
-using AltV.Net.Async;
 
 namespace ResurrectionRP_Server.Society.Societies.Bennys
 {
@@ -25,44 +23,52 @@ namespace ResurrectionRP_Server.Society.Societies.Bennys
         }
         #endregion
 
-        #region Events
+        #region Event handlers
         private async Task OnInteractWithPnj(IPlayer client, Entities.Peds.Ped npc)
         {
             if (_vehicleBench != null)
-                await OpenMainMenu(client, _vehicleBench);
+                await OpenMainMenu(client);
             else
                 client.SendNotificationError("Aucun véhicule devant l'établi.");
         }
 
-        public override void OnPlayerEnterServiceColshape(IColShape colShape, IPlayer client)
+        public void OnVehicleEnterWorkzone(IColShape colShape, IVehicle vehicle)
         {
-            if (!client.Exists)
+            if (vehicle == null || !vehicle.Exists)
                 return;
 
-            if (colShape == _workZone)
-            {
-                if (client.IsInVehicle)
-                    _vehicleBench = client.Vehicle;
-            }
+            if (_vehicleBench == null)
+                _vehicleBench = vehicle;
+        }
 
-            base.OnPlayerEnterServiceColshape(colShape, client);
+        public void OnVehicleLeaveWorkzone(IColShape colShape, IVehicle vehicle)
+        {
+            if (vehicle == null || !vehicle.Exists)
+                return;
+
+            if (vehicle == _vehicleBench)
+                _vehicleBench = null;
         }
         #endregion
 
-        #region Methods
+        #region Init
         public override async Task Init()
         {
             Location pnjPos = new Location(new Vector3(-227.6015f, -1327.772f, 30.89038f), new Vector3(0, 0, 239.715f));
-            var npc = Entities.Peds.Ped.CreateNPC(PedModel.Benny, pnjPos.Pos, pnjPos.Rot.Z, GameMode.GlobalDimension);
+            Ped npc = Ped.CreateNPC(PedModel.Benny, pnjPos.Pos, pnjPos.Rot.Z, GameMode.GlobalDimension);
             npc.NpcInteractCallBack = OnInteractWithPnj;
 
             Vector3 workZonePos = new Vector3(-222.3765f, -1329.64f, 30.46614f);
-            _workZone =  Alt.CreateColShapeCylinder(new Vector3(workZonePos.X, workZonePos.Y, workZonePos.Z - 1), 10, 5);
+            _workZone = Alt.CreateColShapeCylinder(new Vector3(workZonePos.X, workZonePos.Y, workZonePos.Z - 1), 10, 5);
+            _workZone.SetOnVehicleEnterColShape(OnVehicleEnterWorkzone);
+            _workZone.SetOnVehicleLeaveColShape(OnVehicleLeaveWorkzone);
             _garageType = GarageType.Car;
 
             await base.Init();
         }
+        #endregion
 
+        #region Methods
         private void HornPreview(IVehicle vehicle, byte horn)
         {
             foreach (IPlayer client in vehicle.GetPlayersInRange(10f))

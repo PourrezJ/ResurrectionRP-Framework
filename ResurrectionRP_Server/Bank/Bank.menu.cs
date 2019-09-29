17ec2d7626ab1013e82bcfb8b7ecf333aaa0fe09
Le Shred <fred777@free.fr>
2019-09-29 13:13:46 +0200

    ATM: Account History is back

--- ResurrectionRP_Server/Bank/Bank.menu.cs
+++ ResurrectionRP_Server/Bank/Bank.menu.cs
@@ -85,10 +85,9 @@ namespace ResurrectionRP_Server.Bank
             #endregion
 
             #region Account History
-            /*
             var historyItem = new MenuItem("Historique", executeCallback: true);
             historyItem.OnMenuItemCallback += OnHistoryAccount;
-            _bankMenu.Add(historyItem);*/
+            _bankMenu.Add(historyItem);
             #endregion
 
             #region Withdraw

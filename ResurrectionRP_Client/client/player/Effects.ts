2e9ee2b13e9721797d1ef81abba596b2098f990e
Djoe_ <Flashxbox41@gmail.com>
2019-10-06 17:11:27 +0200

    Add alcohol effect

--- ResurrectionRP_Client/client/player/Effects.ts
+++ ResurrectionRP_Client/client/player/Effects.ts
@@ -6,11 +6,17 @@ import * as utils from '../Utils/Utils'
 export class Effects
 {
     constructor() {
-        alt.onServer("AlcoholDrink", this.AlcoholDrink.bind);
+        alt.onServer("AlcoholDrink", this.AlcoholDrink.bind(this));
     }
 
-    private AlcoholDrink() : void {
+    private async AlcoholDrink(quantity: number): Promise<void>
+    {
+        if (game.hasAnimDictLoaded('MOVE_M@DRUNK@VERYDRUNK'))
+            await utils.loadAnim('MOVE_M@DRUNK@VERYDRUNK');
 
-        
+        game.setPedIsDrunk(alt.Player.local.scriptID, true);
+        game.shakeGameplayCam('DRUNK_SHAKE', quantity);
+        game.setPedConfigFlag(alt.Player.local.scriptID, 100, true);
+        game.setPedMovementClipset(alt.Player.local.scriptID, 'MOVE_M@DRUNK@VERYDRUNK', quantity);
     }
 }
--- ResurrectionRP_Server/Items/Alcohol.cs
+++ ResurrectionRP_Server/Items/Alcohol.cs
@@ -1,7 +1,7 @@
-﻿using AltV.Net.Elements.Entities;
+﻿using AltV.Net.Async;
+using AltV.Net.Elements.Entities;
 using ResurrectionRP_Server.Models;
 using System.Numerics;
-using System.Threading.Tasks;
 using Flags = ResurrectionRP_Server.Utils.Enums.AnimationFlags;
 
 namespace ResurrectionRP_Server.Items
@@ -10,8 +10,9 @@ namespace ResurrectionRP_Server.Items
     {
         public int Timer = 60000;
         public int Drink;
+        public int Alcolhol;
 
-        public Alcohol(Models.InventoryData.ItemID id, string name, string description, int weight = 0, bool isGiven = false, bool isUsable = false, bool isStackable = true, bool isDropable = true, bool isDockable = false, int itemPrice = 0, string type = "alcohol", string icon = "unknown-item", string classes = "basic", int timer = 60000, int drink = 0) : base(id, name, description, weight, isGiven, isUsable, isStackable, isDropable, isDockable, itemPrice, type, icon, classes)
+        public Alcohol(Models.InventoryData.ItemID id, string name, string description, int weight = 0, bool isGiven = false, bool isUsable = false, bool isStackable = true, bool isDropable = true, bool isDockable = false, int itemPrice = 0, string type = "alcohol", string icon = "unknown-item", string classes = "basic", int timer = 60000, int drink = 0, double alcohol = 0.1) : base(id, name, description, weight, isGiven, isUsable, isStackable, isDropable, isDockable, itemPrice, type, icon, classes)
         {
             Timer = timer;
             Drink = drink;
@@ -31,38 +32,17 @@ namespace ResurrectionRP_Server.Items
 
                 }
 
-                switch (id)
-                {
-                    case Models.InventoryData.ItemID.Cafe:
-                        AnimateEatDrink(client, ph, "prop_food_coffee", new Vector3(), new Vector3());
-                        break;
-
-                    case Models.InventoryData.ItemID.JambonBeurre:
-                        AnimateEatDrink(client, ph, "prop_sandwich_01", new Vector3(), new Vector3());
-                        break;
-
-                    case Models.InventoryData.ItemID.Donuts:
-                        AnimateEatDrink(client, ph, "prop_donut_01", new Vector3(), new Vector3());
-                        break;
-
-                    case Models.InventoryData.ItemID.Eau:
-                        AnimateEatDrink(client, ph, "prop_ld_flow_bottle", new Vector3(), new Vector3());
-                        break;
-
-                    case Models.InventoryData.ItemID.Vin:
-                        AnimateEatDrink(client, ph, "prop_wine_bot_01", new Vector3(), new Vector3());
-                        break;
-                }
+                AnimateEatDrink(client, ph, "prop_wine_bot_01", new Vector3(), new Vector3());
+                ph.AddAlcolhol(1);
             }
         }
 
         public void AnimateEatDrink(IPlayer client, Entities.Players.PlayerHandler ph, string props, Vector3 position, Vector3 rotation)
         {
             client.PlayAnimation("mp_player_intdrink", "loop_bottle", 4, -8, -1, (Flags.OnlyAnimateUpperBody | Flags.AllowPlayerControl));
-
             Utils.Utils.Delay(4000, async () =>
             {
-                if (!client.Exists)
+                if (!await client.ExistsAsync())
                     return;
 
                 await client.PlayAnimationAsync("mp_player_intdrink", "outro_bottle", 4, -8, -1, (Flags.OnlyAnimateUpperBody | Flags.AllowPlayerControl));

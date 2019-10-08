89aa28e451288188a368d0e0d29c6acb37cd17cf
Djoe_ <Flashxbox41@gmail.com>
2019-10-08 13:08:28 +0200

    tempory inventory fix

--- ResurrectionRP_Client/client/menus/rpgInventory/RPGInventory.ts
+++ ResurrectionRP_Client/client/menus/rpgInventory/RPGInventory.ts
@@ -58,22 +58,60 @@ export class RPGInventoryManager {
 
             alt.log("debug inventaire: création events.");
             this.view.on('inventoryUseItem', (arg1: any, arg2: any, arg3: any) => {
-                alt.log("RPGInventory_UseItem");
+                if (time < 100) {
+                    alt.logWarning('Double call inventoryUseItem: ' + time + 'ms');
+                    return;
+                }
                 alt.emitServer("RPGInventory_UseItem", arg1, arg2, arg3);
             });
 
-            this.view.on('inventoryDropItem', (arg1: any, arg2: any, arg3: any, arg4: any) => alt.emitServer("RPGInventory_DropItem", arg1, arg2, arg3, arg4));
-            this.view.on('inventoryChangeItemPrice', (arg1: any, arg2: any, arg3: any, arg4: any) => alt.emitServer("RPGInventory_PriceItemInventory_SRV", arg1, arg2, arg3, arg4));
-            this.view.on('inventoryGiveItem', (arg1: any, arg2: any, arg3: any, arg4: any) => alt.emitServer("RPGInventory_GiveItem", arg1, arg2, arg3, arg4));  
+            this.view.on('inventoryDropItem', (arg1: any, arg2: any, arg3: any, arg4: any) => {
+                if (time < 100) {
+                    alt.logWarning('Double call inventoryDropItem: ' + time + 'ms');
+                    return;
+                }
+                alt.emitServer("RPGInventory_DropItem", arg1, arg2, arg3, arg4);
+            });
+
+            this.view.on('inventoryChangeItemPrice', (arg1: any, arg2: any, arg3: any, arg4: any) => {
+                if (time < 100) {
+                    alt.logWarning('Double call inventoryChangeItemPrice: ' + time + 'ms');
+                    return;
+                }
+                alt.emitServer("RPGInventory_PriceItemInventory_SRV", arg1, arg2, arg3, arg4);
+            });
+
+            this.view.on('inventoryGiveItem', (arg1: any, arg2: any, arg3: any, arg4: any) => {
+                if (time < 100) {
+                    alt.logWarning('Double call inventoryGiveItem: ' + time + 'ms');
+                    return;
+                }
+                alt.emitServer("RPGInventory_GiveItem", arg1, arg2, arg3, arg4);
+            });  
+
             this.view.on('inventorySwitchItem', (arg1: any, arg2: any, arg3: any, arg4: any, arg5: any) => {
-                alt.log(`debug inventaire: inventorySwitchItem ${arg1} ${arg2} ${arg3} ${arg4} ${arg5}`);
+                if (time < 100) {
+                    alt.logWarning('Double call inventorySwitchItem: ' + time + 'ms');
+                    return;
+                }
                 alt.emitServer("RPGInventory_SwitchItemInventory_SRV", arg1, arg2, arg3, arg4, arg5);
             });
 
-            this.view.on('inventorySplitItem', (arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any) => alt.emitServer("RPGInventory_SplitItemInventory_SRV", arg1, arg2, arg3, arg4, arg5, arg6, arg7));
+            this.view.on('inventorySplitItem', (arg1: any, arg2: any, arg3: any, arg4: any, arg5: any, arg6: any, arg7: any) => {
+                if (time < 100) {
+                    alt.logWarning('Double call inventorySplitItem: ' + time + 'ms');
+                    return;
+                }
+                alt.emitServer("RPGInventory_SplitItemInventory_SRV", arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+            });
 
             // Callback une fois l'inventaire ouvert
             this.view.on("loaditem", () => {
+                if (time < 100) {
+                    alt.logWarning('Double call loaditem: ' + time + 'ms');
+                    return;
+                }
+
                 if (!this.view)
                     return;
 

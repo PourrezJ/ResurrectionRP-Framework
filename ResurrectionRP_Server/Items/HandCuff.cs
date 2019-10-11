4d33260725e3f97b0c2283a374e04962529e6656
Djoe_ <43145127+PourrezJ@users.noreply.github.com>
2019-10-11 23:12:07 +0200

    Phone destroy fix?

--- ResurrectionRP_Server/Items/HandCuff.cs
+++ ResurrectionRP_Server/Items/HandCuff.cs
@@ -1,20 +1,18 @@
 ﻿using AltV.Net.Elements.Entities;
-using ResurrectionRP_Server.Entities.Players;
 using ResurrectionRP_Server.Models;
 using ResurrectionRP_Server.Models.InventoryData;
-using System.Threading.Tasks;
 
 namespace ResurrectionRP_Server.Items
 {
     class HandCuff : Item
     {
-        public HandCuff(Models.InventoryData.ItemID id, string name, string description, double weight = 0, bool isGiven = false, bool isUsable = false, bool isStackable = true, bool isDropable = true, bool isDockable = false, int itemPrice = 0, string type = "handcuff", string icon = "unknown-item", string classes = "basic") : base(id, name, description, weight, isGiven, isUsable, isStackable, isDropable, isDockable, itemPrice, type, icon, classes)
+        public HandCuff(ItemID id, string name, string description, double weight = 0, bool isGiven = false, bool isUsable = false, bool isStackable = true, bool isDropable = true, bool isDockable = false, int itemPrice = 0, string type = "handcuff", string icon = "unknown-item", string classes = "basic") : base(id, name, description, weight, isGiven, isUsable, isStackable, isDropable, isDockable, itemPrice, type, icon, classes)
         {
         }
 
         public override void Use(IPlayer Client, string inventoryType, int slot)
         {
-            if (this.id == ItemID.Serflex || this.name == "Serflex")
+            if (id == ItemID.Serflex || name == "Serflex")
             {
                 var ph = Client.GetPlayerHandler();
 

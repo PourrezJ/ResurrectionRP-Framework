50ea0e3c45db275e19604a5853eaa37346092d5e
Le Shred <fred777@free.fr>
2019-10-13 10:33:57 +0200

    Post merge fix

--- ResurrectionRP_Server/Gamemode.cs
+++ ResurrectionRP_Server/Gamemode.cs
@@ -230,8 +230,7 @@ namespace ResurrectionRP_Server
                     float x = Convert.ToSingle(args[0].ToString().Replace('f',' ').Replace(',',' '));
                     float y = Convert.ToSingle(args[1].ToString().Replace('f', ' ').Replace(',', ' '));
                     float z = Convert.ToSingle(args[2].ToString().Replace('f', ' ').Replace(',', ' '));
-
-                    await player.SetPositionAsync(new Position(x, y, z));
+                    player.Position = new Position(x, y, z);
                 }
                 catch (Exception ex)
                 {

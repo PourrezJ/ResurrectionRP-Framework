af36c4bb45fb0851c030d2e6ea748949fc4f8e3f
alburgoni <alexandre.asto63@orange.fr>
2019-10-28 13:53:34 +0100

    Code Readability (Visual Studio Cleanup)

--- ResurrectionRP_Server/Farms/InteractionPoint.cs
+++ ResurrectionRP_Server/Farms/InteractionPoint.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Numerics;
 using AltV.Net;
 using AltV.Net.Elements.Entities;
 using ResurrectionRP_Server.Colshape;
@@ -8,6 +5,8 @@ using ResurrectionRP_Server.Entities;
 using ResurrectionRP_Server.Entities.Players;
 using ResurrectionRP_Server.Items;
 using ResurrectionRP_Server.Models;
+using System.Collections.Generic;
+using System.Numerics;
 
 namespace ResurrectionRP_Server.Farms
 {
@@ -21,6 +20,7 @@ namespace ResurrectionRP_Server.Farms
     }
     public class InteractionPoint
     {
+        #region Fields
         private Farm Farm;
         public Vector3 Position;
         public float Heading;
@@ -31,7 +31,9 @@ namespace ResurrectionRP_Server.Farms
         public List<Item> ToolNeeded;
         public InteractionPointTypes Type;
         public string InteractionName;
+        #endregion
 
+        #region Constructor
         public InteractionPoint(Farm farm, Vector3 position, float heading, List<Item> items, InteractionPointTypes interactionPoint, string interactionName, float doubleLuck)
         {
             Position = position;
@@ -66,7 +68,8 @@ namespace ResurrectionRP_Server.Farms
             Heading = heading;
             Init();
         }
-
+        #endregion
+        #region Init
         private void Init()
         {
             Colshape = ColshapeManager.CreateCylinderColshape(Position - new Vector3(0, 0, 1), 2, 3);
@@ -75,8 +78,10 @@ namespace ResurrectionRP_Server.Farms
 
             Marker.CreateMarker(MarkerType.MarkerTypeHorizontalBars, Position + new Vector3(0, 0, 1), new Vector3(1, 1, 1), System.Drawing.Color.FromArgb(128, 255, 69, 0));
         }
+        #endregion
 
-        private void Colshape_OnPlayerInteractInColshape(IColshape colshape, AltV.Net.Elements.Entities.IPlayer client)
+        #region Event Handlers
+        private void Colshape_OnPlayerInteractInColshape(IColshape colshape, IPlayer client)
         {
             if (client.IsInVehicle)
             {
@@ -93,7 +98,7 @@ namespace ResurrectionRP_Server.Farms
                 {
                     PlayerHandler _client = client.GetPlayerHandler();
                     Inventory.Inventory inventory = _client.HasItemInAnyInventory(_item.id);
-                    Models.ItemStack item = _client.OutfitInventory.HasItemEquip(_item.id);
+                    ItemStack item = _client.OutfitInventory.HasItemEquip(_item.id);
                     if (item != null)
                     {
                         if (item.Item.type == "tool" && (item.Item as Tool).Health <= 0)
@@ -106,9 +111,7 @@ namespace ResurrectionRP_Server.Farms
                     }
 
                     if (inventory != null && item == null)
-                    {
                         client.DisplayHelp("Vous devez équiper votre outil pour commencer!", 5000);
-                    }
                     else if (item == null && ToolNeeded.IndexOf(_item) == ToolNeeded.Count - 1)
                     {
                         client.DisplayHelp($"Vous devez avoir un(e) {ToolNeeded[0].name} pour {InteractionName} !", 10000);
@@ -124,26 +127,27 @@ namespace ResurrectionRP_Server.Farms
             }
         }
 
-        private void Colshape_OnPlayerEnterColshape(IColshape colshape, AltV.Net.Elements.Entities.IPlayer client)
+        private void Colshape_OnPlayerEnterColshape(IColshape colshape, IPlayer client)
         {
             if (client.IsInVehicle)
             {
                 client.DisplayHelp("Vous ne pouvez faire être ici avec un véhicule!", 5000);
                 return;
             }
-            if(ToolNeeded.Count == 0) 
+            if (ToolNeeded.Count == 0)
                 client.DisplayHelp("Appuyez sur ~INPUT_CONTEXT~ pour commencer à " + InteractionName, 5000);
 
             try
             {
-                foreach(Item _item in ToolNeeded) 
+                foreach (Item _item in ToolNeeded)
                 {
                     PlayerHandler _client = client.GetPlayerHandler();
                     Inventory.Inventory inventory = _client.HasItemInAnyInventory(_item.id);
-                    Models.ItemStack item = _client.OutfitInventory.HasItemEquip(_item.id);
-                    if(item != null)
+                    ItemStack item = _client.OutfitInventory.HasItemEquip(_item.id);
+
+                    if (item != null)
                     {
-                        if (item.Item.type == "tool"  && (item.Item as Tool).Health <= 0)
+                        if (item.Item.type == "tool" && (item.Item as Tool).Health <= 0)
                         {
                             _client.OutfitInventory.Delete(item, 1);
                             client.DisplayHelp("Votre outil s'est cassé, vous êtes bon pour en racheter un !", 10000);
@@ -156,9 +160,7 @@ namespace ResurrectionRP_Server.Farms
 
 
                     if (inventory != null && item == null)
-                    {
                         client.DisplayHelp("Vous devez équiper votre outil pour commencer!", 5000);
-                    }
                     else if (item == null && ToolNeeded.IndexOf(_item) == ToolNeeded.Count - 1)
                     {
                         client.DisplayHelp($"Vous devez avoir un(e) {ToolNeeded[0].name} pour {InteractionName} !", 10000);
@@ -174,7 +176,9 @@ namespace ResurrectionRP_Server.Farms
             }
 
         }
+        #endregion
 
+        #region Methods
         private void LaunchToFarm(IPlayer client)
         {
             PlayerHandler _client = client.GetPlayerHandler();
@@ -201,6 +205,7 @@ namespace ResurrectionRP_Server.Farms
                     return;
             }
         }
+        #endregion
     }
 
 }

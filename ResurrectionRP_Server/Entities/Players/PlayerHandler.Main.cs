38135bab7539905b57ab0eaa828e7b4f6d40000f
alburgoni <alexandre.asto63@orange.fr>
2019-10-29 11:51:27 +0100

    ADD logs for Hunger / Thirst

--- ResurrectionRP_Server/Entities/Players/PlayerHandler.Main.cs
+++ ResurrectionRP_Server/Entities/Players/PlayerHandler.Main.cs
@@ -87,8 +87,8 @@ namespace ResurrectionRP_Server.Entities.Players
         public OutfitInventory OutfitInventory { get; set; } = new Inventory.OutfitInventory();
         public double Money { get; private set; }
         public Bank.BankAccount BankAccount { get; set; }
-        public int Hunger { get; set; } = 100;
-        public int Thirst { get; set; } = 100;
+        public int Hunger { get; set; }
+        public int Thirst { get; set; }
 
         public double Alcohol = 0;
         public bool Jailed { get; private set; } = false;
@@ -221,6 +221,7 @@ namespace ResurrectionRP_Server.Entities.Players
                     if (Client.Model != ((Character.Gender == 0) ? (uint)AltV.Net.Enums.PedModel.FreemodeMale01 : (uint)AltV.Net.Enums.PedModel.FreemodeFemale01))
                         Client.Model = (Character.Gender == 0) ? (uint)AltV.Net.Enums.PedModel.FreemodeMale01 : (uint)AltV.Net.Enums.PedModel.FreemodeFemale01;
 
+                    Alt.Server.LogInfo($"[HUNGER] {PID} : Init Player hunger ({Hunger}) thirst({Thirst})");
                     Client.Emit
                     (
                         Events.PlayerInitialised,
@@ -359,7 +360,7 @@ namespace ResurrectionRP_Server.Entities.Players
             Thirst = (thirst == -1) ? Thirst : thirst;
             Hunger = (hunger == -1) ? Hunger : hunger;
             UpdateFull();
-
+            Alt.Server.LogInfo($"[HUNGER] {PID} new update hunger ({Thirst}) thirst({Hunger})");
             if (Client != null && Client.Exists)
                 Client.EmitLocked("UpdateHungerThirst", Hunger, Thirst);
 
